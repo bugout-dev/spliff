2c382d14997b72de0251d4b28cb01f475600253b
yhtiyar <yhtiyarsahatow@gmail.com>
2021-08-11 21:25:09 +0300

    addind solana calls

--- src/token.rs
+++ src/token.rs
@@ -3,6 +3,10 @@ use rocket::State;
 use serde::{Deserialize, Serialize};
 use serde_json;
 use solana_client::rpc_request::TokenAccountsFilter;
+use solana_sdk::{
+    pubkey::Pubkey,
+    signer::{keypair::Keypair, Signer},
+};
 use spl_token;
 
 use super::state::SolanaClient;
@@ -32,6 +36,12 @@ pub fn list_tokens(solana_client: &State<SolanaClient>) -> String {
     return serde_json::json!(accounts).to_string();
 }
 
+fn new_throwaway_signer() -> (Box<dyn Signer>, Pubkey) {
+    let keypair = Keypair::new();
+    let pubkey = keypair.pubkey();
+    (Box::new(keypair) as Box<dyn Signer>, pubkey)
+}
+
 #[derive(Deserialize, Debug)]
 pub struct TokenSuply {
     supply: u64,
@@ -39,6 +49,15 @@ pub struct TokenSuply {
 #[rocket::post("/", data = "<token_supply>")]
 pub fn create_token(token_supply: Json<TokenSuply>, solana_client: &State<SolanaClient>) {
     println!("{:?}", token_supply.into_inner());
+    let (token_signer, token) = new_throwaway_signer();
+    // Also spl_token::initialize_accaut()
+    solana_sdk::system_instruction::create_account(
+        from_pubkey: &Pubkey, //fee payer
+        &token,
+        lamports: u64,
+        space: u64,
+        &spl_token::id(), //owner
+    );
     unimplemented!();
 }
 
@@ -48,8 +67,26 @@ pub struct TokenTransferRequest {
     recipient: String,
     amount: u64,
 }
-#[rocket::post("/transfer", data = "<token_transfer_request>")]
-pub fn transfer_token(token_transfer_request: Json<TokenTransferRequest>) {
-    println!("{:?}", token_transfer_request.into_inner());
+#[rocket::post("/transfer", data = "<token_transfer_request_json>")]
+pub fn transfer_token(
+    token_transfer_request_json: Json<TokenTransferRequest>,
+    solana_client: &State<SolanaClient>,
+) {
+    println!("{:?}", token_transfer_request_json.into_inner());
+
+    let token_transfer_request = token_transfer_request_json.into_inner();
+    let token = Pubkey::new(token_transfer_request.token.as_bytes());
+    let destination = Pubkey::new(token_transfer_request.recipient.as_bytes());
+
+    //solana_sdk::system_instruction::transfer(from_pubkey: &Pubkey, to_pubkey: &Pubkey, lamports: u64)
+
+    spl_token::instruction::transfer(
+        &token,                //Token program id
+        &solana_client.pubkey, //source_pubkey
+        &destination,          //Destination pubkey
+        authority_pubkey: &Pubkey,
+        signer_pubkeys: &[&Pubkey],
+        token_transfer_request.amount, //amount
+    );
     unimplemented!();
 }
